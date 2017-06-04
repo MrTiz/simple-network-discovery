77d8843bb89198556589f1e2ba8f113ae87242e2
MrTiz <tiziano@mrtiz.net>
2017-06-04 16:47:42 +0200

    Fix bug if a device is unreachable

--- src/main.c
+++ src/main.c
@@ -443,7 +443,7 @@ int bind_arp(int ifindex, int *fd) {
  */
 void set_socket_timeout(int * fd) {
     struct timeval timeout;      
-    timeout.tv_sec = 3;
+    timeout.tv_sec = 5;
     timeout.tv_usec = 0;
 
     setsockopt (*fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
